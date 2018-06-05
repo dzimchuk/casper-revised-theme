759569eab09a625a7419920dac9c322371b00d1c
Andrei Dzimchuk <andrei@dzimchuk.net>
2018-06-05 22:00:10 +0300

    more style cleanup

--- src/js/site.js
+++ src/js/site.js
@@ -1,21 +1,9 @@
-/**
- * Main JS file for Casper behaviours
- */
-
-/* globals jQuery, document */
 (function ($, undefined) {
     "use strict";
 
     var $document = $(document);
 
     $document.ready(function () {
-
-        $(".scroll-down").arctic_scroll();
-
-        $(".menu-button, .nav-cover, .nav-close").on("click", function(e){
-            e.preventDefault();
-            $("body").toggleClass("nav-opened nav-closed");
-        });
 
         function updateLink(className, url){
             var link = $(className);
@@ -28,35 +16,4 @@
         if (typeof google_plus_url !== 'undefined') { updateLink('.social-google-plus', google_plus_url); }
     });
 
-    // Arctic Scroll by Paul Adam Davis
-    // https://github.com/PaulAdamDavis/Arctic-Scroll
-    $.fn.arctic_scroll = function (options) {
-
-        var defaults = {
-            elem: $(this),
-            speed: 500
-        },
-
-        allOptions = $.extend(defaults, options);
-
-        allOptions.elem.click(function (event) {
-            event.preventDefault();
-            var $this = $(this),
-                $htmlBody = $('html, body'),
-                offset = ($this.attr('data-offset')) ? $this.attr('data-offset') : false,
-                position = ($this.attr('data-position')) ? $this.attr('data-position') : false,
-                toMove;
-
-            if (offset) {
-                toMove = parseInt(offset);
-                $htmlBody.stop(true, false).animate({scrollTop: ($(this.hash).offset().top + toMove) }, allOptions.speed);
-            } else if (position) {
-                toMove = parseInt(position);
-                $htmlBody.stop(true, false).animate({scrollTop: toMove }, allOptions.speed);
-            } else {
-                $htmlBody.stop(true, false).animate({scrollTop: ($(this.hash).offset().top) }, allOptions.speed);
-            }
-        });
-
-    };
 })(jQuery);
